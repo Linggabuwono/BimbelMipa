4c7b50476a14caa3e4f716233f5a975dc25503d9
Muhammad Ferdy <ferdyzecter@gmail.com>
2021-07-19 22:09:25 +0700

    2nd Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Bimbel MIPA</title>
+    <title>BimbelMIPA</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
